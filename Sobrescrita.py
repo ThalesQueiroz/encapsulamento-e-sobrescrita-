9971be42cc616fd0c6bb9b081da73d5bde8103c5
ThalesQueiroz <148828539+ThalesQueiroz@users.noreply.github.com>
2023-10-23 21:51:28 -0300

    Update Sobrescrita.py

--- Sobrescrita.py
+++ Sobrescrita.py
@@ -1,1 +1,19 @@
+class Integrante_IFRN:
+    def __init__(self, ):
+    
+    def exibirMensagem(self):
+        print ('Seja bem vindo(a) ao IFRN!!!')
+        
+class Professor(Integrante_IFRN):
+    def __init__(self, ):
+    def super().__init__(self):
 
+    def exibirMensagem(self):
+        print ('Meus alunos são os melhores!!!')
+
+class Aluno(Integrante_IFRN):
+    def __init__(self, ):
+    def super().__init__(self):
+    
+    def exibirMensagem(self):
+        print ('Vou estudar para tirar 100 em POO!!!')
